3ddd78d910e6901961b3e487b0beb70cbe7fc3a7
SashaKarpo <prmzwe@gmail.com>
2023-01-27 22:48:30 +0300

    expended venv show_category

--- women/views.py
+++ women/views.py
@@ -50,4 +50,17 @@ def show_post(request, post_id):
 
 
 def show_category(request, cat_id):
-    return HttpResponse(f'Категория с индефикатором {cat_id}')
+    posts = Women.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+
+    if len(posts) == 0:
+        raise Http404
+
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title': 'Отображение по рубрикам',
+        'cat_selected': cat_id,
+    }
+    return render(request, 'women/index.html', context=context)
