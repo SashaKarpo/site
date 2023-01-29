b88a7d461c03e16a40965d40ea7143c4a23601d1
SashaKarpo <prmzwe@gmail.com>
2023-01-29 15:44:44 +0300

    created class CategoryAdmin

--- women/admin.py
+++ women/admin.py
@@ -9,4 +9,11 @@ class WomenAdmin(admin.ModelAdmin):
     search_fields = ('title', 'content')
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+
+
 admin.site.register(Women, WomenAdmin)
+admin.site.register(Category, CategoryAdmin)
