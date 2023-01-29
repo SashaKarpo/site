0f082bff99537c4dcf40572d5f6d39328308bdfa
SashaKarpo <prmzwe@gmail.com>
2023-01-29 22:03:52 +0300

    add in WomenAdmin

--- women/admin.py
+++ women/admin.py
@@ -7,6 +7,8 @@ class WomenAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
+    list_editable = ('is_published',)
+    list_filter = ('is_published', 'time_create')
 
 
 class CategoryAdmin(admin.ModelAdmin):
