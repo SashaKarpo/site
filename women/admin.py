f061a9f6d2dad43467fce9785cedd8f063ad0b04
SashaKarpo <prmzwe@gmail.com>
2023-01-29 15:29:15 +0300

    add class WomenAdmin

--- women/admin.py
+++ women/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(Women)
+
+class WomenAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'time_created', 'photo', 'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+
+
+admin.site.register(Women, WomenAdmin)
