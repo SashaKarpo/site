fd01da121d47abbd142cf01ca9516f5866ec2f0f
SashaKarpo <prmzwe@gmail.com>
2023-01-30 14:14:19 +0300

    single post display

--- women/views.py
+++ women/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import *
 
@@ -44,7 +44,15 @@ def pageNotFound(request, exception):
 
 
 def show_post(request, post_id):
-    return HttpResponse(f'Статья с индефикатором {post_id}')
+    post = get_object_or_404(Women, pk=post_id)
+
+    context = {
+        'post': post,
+        'menu': menu,
+        'title': post.title,
+        'cat_selected': post.cat_id,
+    }
+    return render(request, 'women/post.html', context=context)
 
 
 def show_category(request, cat_id):
