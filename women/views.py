b325cd039b14a4d68617b16641889ac70089af9b
SashaKarpo <prmzwe@gmail.com>
2023-01-31 13:57:59 +0300

    bound the form to the database

--- women/views.py
+++ women/views.py
@@ -32,7 +32,12 @@ def addpage(request):
     if request.method == 'POST':
         form = AddPostForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
+            # print(form.cleaned_data)
+            try:
+                Women.objects.create(**form.cleaned_data)
+                return redirect('home')
+            except:
+                form.add_error(None, 'Ошибка добавления поста')
     else:
         form = AddPostForm()
     return render(request, 'women/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'})
