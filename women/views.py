6b92ac0309f1db0680f885ec2d5ca43df9c5c4a0
SashaKarpo <prmzwe@gmail.com>
2023-01-31 14:54:58 +0300

    made a connection between the form and the table Women

--- women/views.py
+++ women/views.py
@@ -30,14 +30,11 @@ def about(request):
 
 def addpage(request):
     if request.method == 'POST':
-        form = AddPostForm(request.POST)
+        form = AddPostForm(request.POST, request.FILES)
         if form.is_valid():
             # print(form.cleaned_data)
-            try:
-                Women.objects.create(**form.cleaned_data)
-                return redirect('home')
-            except:
-                form.add_error(None, 'Ошибка добавления поста')
+            form.save()
+            return redirect('home')
     else:
         form = AddPostForm()
     return render(request, 'women/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'})
