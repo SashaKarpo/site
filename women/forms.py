3b33ead4509616fa18da090a9daf47577a40e598
SashaKarpo <prmzwe@gmail.com>
2023-02-06 11:48:49 +0300

    added user registration on the site

--- women/forms.py
+++ women/forms.py
@@ -26,11 +26,12 @@ class AddPostForm(forms.ModelForm):
 
 
 class RegisterUserForm(UserCreationForm):
+    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-input'}))
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    password2 = forms.CharField(label='Пароль ещё раз', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+
     class Meta:
         model = User
         fields = ('username', 'password1', 'password2')
-        widgets = {
-            'username': forms.TextInput(attrs={'class': 'form-input'}),
-            'password1': forms.PasswordInput(attrs={'class': 'form-input'}),
-            'password2': forms.PasswordInput(attrs={'class': 'form-input'})
-        }
+
