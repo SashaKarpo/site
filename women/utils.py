2c194ff75583a7d473840923d7f75a2cbe7735f4
SashaKarpo <prmzwe@gmail.com>
2023-02-07 13:16:35 +0300

    optimized the site reduced the number of requests to display the page

--- women/utils.py
+++ women/utils.py
@@ -9,7 +9,7 @@ menu = [{'title': "О сайте", 'url_name': 'about'},
 
 
 class DataMixin:
-    paginate_by = 3
+    paginate_by = 20
 
     def get_user_context(self, **kwargs):
         context = kwargs
